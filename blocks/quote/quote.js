10069a3b576abc8f87f566cb8ed28b9be13da794
Jose Carreno <josecarreno@adobe.com>
2026-05-18 13:47:14 +0200

    creating a callout component

--- blocks/quote/quote.js
+++
@@ -1,6 +0,0 @@
-export default function decorate(block) {
-  const [wrapper] = block.children;
-  const blockQuote = document.createElement('blockquote');
-  blockQuote.textContent = wrapper.textContent.trim();
-  block.replaceChild(blockQuote, wrapper);
-}
