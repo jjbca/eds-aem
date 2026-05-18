8dfc25e180536ba9780e4641ee4e42b9d9ea11f9
Jose Carreno <josecarreno@adobe.com>
2026-05-18 13:36:49 +0200

    fix eslint

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,6 +1,6 @@
 export default function decorate(block) {
-    const [wrapper] = block.children;
-    const blockQuote = document.createElement('blockquote');
-    blockQuote.textContent = wrapper.textContent.trim();
-    block.replaceChild(blockQuote, wrapper);
+  const [wrapper] = block.children;
+  const blockQuote = document.createElement('blockquote');
+  blockQuote.textContent = wrapper.textContent.trim();
+  block.replaceChild(blockQuote, wrapper);
 }
