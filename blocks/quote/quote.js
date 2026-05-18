0bcc73f03121357a110f620f95703e5963f36c61
Jose Carreno <josecarreno@adobe.com>
2026-05-18 09:50:15 +0200

    styling block quote

---
+++ blocks/quote/quote.js
@@ -0,0 +1,6 @@
+export default function decorate(block) {
+    const [wrapper] = block.children;
+    const blockQuote = document.createElement('blockquote');
+    blockQuote.textContent = wrapper.textContent.trim();
+    wrapper.replaceChildren(blockQuote);
+}
